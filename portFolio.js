ab0bee177a3ee70d1cb4bc95cb706703f5523582
Yalagurappa Kulakarni <97602081+YKKulakarni@users.noreply.github.com>
2025-09-30 17:15:21 +0530

    Add files via upload

---
+++ portFolio.js
@@ -0,0 +1,14 @@
+let prevScrollPos = window.scrollY
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navBar")
+    const currentScrollPos = window.scrollY
+    if (currentScrollPos > prevScrollPos) {
+        navbar.classList.add('hide')
+        navbar.classList.remove('show')
+    }
+    else {
+        navbar.classList.add('show')
+        navbar.classList.remove('hide')
+    }
+    prevScrollPos = currentScrollPos
+})
